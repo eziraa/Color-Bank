7acc606fb943adfac288a0a4ef21e9f228fba9f2
eziraa <ezratgab@gmail.com>
2024-01-30 00:44:08 +0300

    generate random color hex balue method added

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const colorContainer = document.querySelector(".color-container");
 const colorHexValue = document.querySelector(".color-hex");
 const btnCopy = document.querySelector(".btn-copy");
 
+const generateColor = function () {
+  const COLOR_HEX_VALUES = "1234567890ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++)
+    color += COLOR_HEX_VALUES.charAt(
+      Math.trunc(Math.random() * COLOR_HEX_VALUES.length)
+    );
+  return color;
+};
 const copyToClipBoard = function (e) {
   if (e.target.classList.contains("btn-copy")) {
     const textToBeCopied = colorHexValue.textContent;
