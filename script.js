b8ad8ab5c08ebd3ad1af78b4ca62427f1fa08475
eziraa <ezratgab@gmail.com>
2024-01-30 00:35:38 +0300

    copy color hex value to clip board

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // getting necessary element
 
+const colorContainer = document.querySelector(".color-container");
 const colorHexValue = document.querySelector(".color-hex");
 const btnCopy = document.querySelector(".btn-copy");
 
+const copyToClipBoard = function (e) {
+  if (e.target.classList.contains("btn-copy")) {
+    const textToBeCopied = colorHexValue.textContent;
+    navigator.clipboard.writeText(textToBeCopied);
+    alert(`${colorHexValue.textContent} copied`);
+  }
+};
 
-btnCopy.addEventListener("click", copyToClipBoard);
+colorContainer.addEventListener("click", copyToClipBoard);
+
